bf276a687769bd63d3d8750ff244685d2d3759b6
nuriasantossanchez <nuria.santossanchez@gmail.com>
2020-08-21 22:23:02 +0200

    exercici restaurant - fase3

---
+++ ExerciciRestaurant/src/RestaurantApp.java
@@ -0,0 +1,118 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import fase1.UtilsFase1;
+import fase2.UtilsFase2;
+import fase3.UtilsFase3;
+
+public class RestaurantApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\n======================= Fase 1 =======================\n");
+		
+		UtilsFase1 utilsFase1 = new UtilsFase1();
+		System.out.println(utilsFase1.toString());
+		
+		System.out.println("\n======================= Fase 2 =======================\n");
+		
+		UtilsFase2 utilsFase2 = new UtilsFase2();
+		utilsFase2.addPrecioPlato(utilsFase1.getMenuArray(), utilsFase1.getPrecioPlatoArray());
+		System.out.println(utilsFase2.toString());
+		
+		utilsFase2.printMenu();
+		
+		Scanner consoleIn = new Scanner(System.in);
+		ArrayList<String> pedido = new ArrayList<String>();
+		try {
+			pedido = pedirComida(consoleIn,pedido);
+		} catch (Exception e) {
+			System.out.println("Error inesperado");
+			e.getMessage();
+			e.printStackTrace();
+		} finally {
+			System.out.println();
+			System.out.println("-----------");
+			utilsFase2.setPedidoComidaList(pedido);
+			System.out.println("Su pedido: " + utilsFase2.getPedidoComidaList().toString());
+			System.out.println("-----------");
+
+		}
+
+		System.out.println("\n======================= Fase 3 =======================\n");
+		
+		UtilsFase3 utilsFase3 = new UtilsFase3(utilsFase2.getPedidoComidaList());
+		utilsFase3.printTicket(utilsFase2.getPrecioMenuHashMap(), utilsFase3.getPedidoComida(), utilsFase1);
+		
+	}
+
+	@SuppressWarnings("finally")
+	public static ArrayList<String> pedirComida(Scanner consoleIn, ArrayList<String> pedido) throws NumberFormatException, EntradaDeDatosEnBlancoException{
+		int opcionMasComida=0;
+		String plato;
+		
+		try {
+			System.out.println("Que quiere comer?");
+			plato = consoleIn.nextLine();
+
+			if (plato.trim().isEmpty()) {
+				throw new EntradaDeDatosEnBlancoException(
+						"Entrada de datos por consola son blancos o es un salto de línea");
+			} else {
+				pedido.add(plato);
+
+				System.out.println("Quiere comer algo mas? (1:Si / 0:No)");
+				opcionMasComida = Integer.parseInt(consoleIn.nextLine());
+				while (opcionMasComida != 1 && opcionMasComida != 0) {
+					System.out.println("La opcion introducida no es correcta");
+					System.out.println("Quiere comer algo mas? (1:Si / 0:No)");
+					opcionMasComida = Integer.parseInt(consoleIn.nextLine());
+				}
+				
+				if (opcionMasComida != 0) {
+					pedirComida(consoleIn, pedido);
+				}
+			}
+		} catch (NumberFormatException e) {
+			System.out.println("La opcion introducida no es un numero. Introduzca un numero, por favor");
+			System.out.println("Quiere comer algo mas? (1:Si / 0:No)");
+			
+			opcionMasComida = Integer.parseInt(consoleIn.nextLine());
+	
+			while (opcionMasComida != 1 && opcionMasComida != 0) {
+				System.out.println("La opcion introducida no es correcta");
+				System.out.println("Quiere comer algo mas? (1:Si / 0:No)");
+				opcionMasComida = Integer.parseInt(consoleIn.nextLine());
+			}
+
+			if (opcionMasComida != 0) {
+				pedirComida(consoleIn, pedido);
+			}
+		} catch (EntradaDeDatosEnBlancoException e) {
+			System.out.println("No ha introducido nada para comer");
+			pedirComida(consoleIn, pedido);
+		} catch (Exception e) {
+			System.out.println("Error inesperado en pedirComida(Scanner consoleIn, ArrayList<String> pedido)");
+			e.getMessage();
+			e.printStackTrace();
+		} finally {
+			return pedido;
+		}
+	}
+
+}
+
+class EntradaDeDatosEnBlancoException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public EntradaDeDatosEnBlancoException() {
+		
+	}
+	
+	public EntradaDeDatosEnBlancoException(String errorMessage) {
+		
+		super(errorMessage);
+	}
+	
+}
---
+++ ExerciciRestaurant/src/fase3/UtilsFase3.java
@@ -0,0 +1,117 @@
+package fase3;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import fase1.UtilsFase1;
+
+public class UtilsFase3 {
+
+	private ArrayList<String> pedidoFueraDeMenu = new ArrayList<String>();
+	private ArrayList<String> pedidoComida = new ArrayList<String>();
+
+	public UtilsFase3(ArrayList<String> pedidoComida) {
+		this.pedidoComida = pedidoComida;
+	}
+
+	public ArrayList<String> getPedidoFueraDeMenu() {
+		return pedidoFueraDeMenu;
+	}
+
+	public void setPedidoFueraDeMenu(ArrayList<String> pedidoFueraDeMenu) {
+		this.pedidoFueraDeMenu = pedidoFueraDeMenu;
+	}
+
+	public ArrayList<String> getPedidoComida() {
+		return pedidoComida;
+	}
+
+	public void setpedidoComida(ArrayList<String> pedidoComida) {
+		this.pedidoComida = pedidoComida;
+	}
+
+	public void damePedidoComidaFueraDeMenu(ArrayList<String> pedidoComida, String[] menu) {
+
+		for (String s : pedidoComida) {
+			int cont = 0;
+			for (int i = 0; i < menu.length; i++) {
+				if (!s.toUpperCase().equals(menu[i].toUpperCase())) {
+					cont++;
+				}
+			}
+			if (cont == menu.length) {
+				this.getPedidoFueraDeMenu().add(s);
+			}
+		}
+
+		if (!this.getPedidoFueraDeMenu().isEmpty()) {
+			System.out.println();
+			System.out.println("-------------------------------------------------------------");
+			for (String s : this.getPedidoFueraDeMenu()) {
+				System.out.println("Ha pedido: " + s.toUpperCase() + ". Este producto no existe en el menu");
+			}
+			System.out.println("-------------------------------------------------------------");
+		}
+
+	}
+
+	public void printTicket(Map<String, Double> menuPrecio, ArrayList<String> pedidoComida,UtilsFase1 utilsFase1) {
+		double precioTotal = 0;
+
+		System.out.println("------------------------------------");
+		System.out.println("Ticket");
+		System.out.println("------------------------------------");
+
+		for (String s : this.getPedidoComida()) {
+			for (Map.Entry<String, Double> entrada : menuPrecio.entrySet()) {
+				String plato = entrada.getKey();
+				double precioPlato = entrada.getValue();
+				if (s.equalsIgnoreCase(plato)) {
+					System.out.println(plato.toUpperCase() + ": " + String.format("%.2f", precioPlato) + " €");
+					precioTotal += precioPlato;
+					break;
+				}
+			}
+		}
+
+		utilsFase1.setPrecioTotal(precioTotal);
+
+		System.out.println("\nTotal a pagar: " + String.format("%.2f", utilsFase1.getPrecioTotal()) + " €\n");
+
+		if (utilsFase1.getPrecioTotal() > 0) {
+			desglosarEnBilletesPrecioTotalTicket(utilsFase1);
+		}
+
+		damePedidoComidaFueraDeMenu(this.getPedidoComida(), utilsFase1.getMenuArray());
+
+	}
+
+	public void desglosarEnBilletesPrecioTotalTicket(UtilsFase1 utilsFase1) {
+		int contBilletes5 = 0, contBilletes10 = 0, contBilletes20 = 0, contBilletes50 = 0, contBilletes100 = 0,
+				contBilletes200 = 0, contBilletes500 = 0;
+		double totalTicket = utilsFase1.getPrecioTotal();
+
+		System.out.println("-----------------------");
+		System.out.println("Opción de pago:");
+		System.out.println("-----------------------");
+
+		totalTicket = desglosandoEnBilletes(totalTicket, utilsFase1.getBillete500€(), contBilletes500);
+		totalTicket = desglosandoEnBilletes(totalTicket, utilsFase1.getBillete200€(), contBilletes200);
+		totalTicket = desglosandoEnBilletes(totalTicket, utilsFase1.getBillete100€(), contBilletes100);
+		totalTicket = desglosandoEnBilletes(totalTicket, utilsFase1.getBillete50€(), contBilletes50);
+		totalTicket = desglosandoEnBilletes(totalTicket, utilsFase1.getBillete20€(), contBilletes20);
+		totalTicket = desglosandoEnBilletes(totalTicket, utilsFase1.getBillete10€(), contBilletes10);
+		totalTicket = desglosandoEnBilletes(totalTicket, utilsFase1.getBillete5€(), contBilletes5);
+
+	}
+
+	public double desglosandoEnBilletes(double totalTicket, int billeteDeDesglose, int contBilletes) {
+		contBilletes = (int) (totalTicket - totalTicket % billeteDeDesglose) / billeteDeDesglose;
+		if (contBilletes > 0) {
+			System.out.println("Billetes de " + billeteDeDesglose + "€: " + contBilletes);
+			totalTicket -= billeteDeDesglose * contBilletes;
+		}
+		return totalTicket;
+	}
+
+}
